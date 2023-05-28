8dc7adffb3082a29c1d55708e5d4358ee22cf2f8
Ayan Sarkar <contact.ayansarkar@gmail.com>
2023-05-28 16:56:21 +0530

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,7 +1,7 @@
 const siteMetadata = {
   title: 'Ayan Sarkar Blog',
   author: 'Tails Azimuth',
-  headerTitle: 'AyanSarkarBlog',
+  headerTitle: 'Ayan Sarkar Blog',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
